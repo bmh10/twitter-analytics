59ea4e89b0c7b2cabced1b45ace638bedd53e124
bmh10 <bmhomer13@gmail.com>
2018-05-20 10:13:16 +0100

    Extract twitter friends

--- src/main/java/com/bmh10/twitteranalytics/TwitterFacade.java
+++ src/main/java/com/bmh10/twitteranalytics/TwitterFacade.java
@@ -7,21 +7,18 @@ public class TwitterFacade {
     public static void main(String[] args) {
         try {
             Twitter twitter = new TwitterFactory().getInstance();
-            AccountSettings settings = twitter.getAccountSettings();
-            System.out.println("Sleep time enabled: " + settings.isSleepTimeEnabled());
-            System.out.println("Sleep end time: " + settings.getSleepEndTime());
-            System.out.println("Sleep start time: " + settings.getSleepStartTime());
-            System.out.println("Geo enabled: " + settings.isGeoEnabled());
-            System.out.println("Screen name: " + settings.getScreenName());
-            System.out.println("Listing trend locations:");
-            Location[] locations = settings.getTrendLocations();
-            for (Location location : locations) {
-                System.out.println(" " + location.getName());
-            }
-            System.exit(0);
+            long cursor = -1;
+            PagableResponseList<User> friends;
+            do {
+                friends = twitter.getFriendsList("realDonaldTrump", cursor, 200);
+                for (User friend : friends) {
+                    System.out.println("@" + friend.getScreenName());
+                }
+            } while ((cursor = friends.getNextCursor()) != 0);
+
         } catch (TwitterException te) {
             te.printStackTrace();
-            System.out.println("Failed to get account settings: " + te.getMessage());
+            System.out.println("Failed to lookup friendships: " + te.getMessage());
             System.exit(-1);
         }
     }
