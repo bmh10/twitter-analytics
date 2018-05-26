3f0cd1c6b8af2315cbb97a8804180b8adf0cfb45
bmh10 <bmhomer13@gmail.com>
2018-05-26 18:08:25 +0100

    Tidy

--- src/main/java/com/bmh10/twitteranalytics/linkedin/LinkedinFacade.java
+++ src/main/java/com/bmh10/twitteranalytics/linkedin/LinkedinFacade.java
@@ -24,145 +24,25 @@ public class LinkedinFacade {
                 .build();
 
         Token accessToken = getAccess(service);
+        printProfile(service, accessToken);
+        printConncetions(service, accessToken);
+    }
 
-        System.out.println();
-        System.out.println("********A basic user profile call********");
-        //The ~ means yourself - so this should return the basic default information for your profile in XML format
-        //https://developer.linkedin.com/documents/profile-api
+    public static void printProfile(OAuthService service, Token accessToken) {
         String url = "http://api.linkedin.com/v1/people/~";
+        OAuthRequest request = new OAuthRequest(Verb.GET, url);
+        request.addHeader("x-li-format", "json");
+        service.signRequest(accessToken, request);
+        Response response = request.send();
+        System.out.println(response.getBody());
+    }
+
+    public static void printConncetions(OAuthService service, Token accessToken) {
+        String url = "http://api.linkedin.com/v1/people/~/connections";
         OAuthRequest request = new OAuthRequest(Verb.GET, url);
         service.signRequest(accessToken, request);
         Response response = request.send();
         System.out.println(response.getBody());
-        System.out.println();System.out.println();
-
-        System.out.println("********Get the profile in JSON********");
-        //This basic call profile in JSON format
-        //You can read more about JSON here http://json.org
-        url = "http://api.linkedin.com/v1/people/~";
-        request = new OAuthRequest(Verb.GET, url);
-        request.addHeader("x-li-format", "json");
-        service.signRequest(accessToken, request);
-        response = request.send();
-        System.out.println(response.getBody());
-        System.out.println();System.out.println();
-
-        System.out.println("********Get the profile in JSON using query parameter********");
-        //This basic call profile in JSON format. Please note the call above is the preferred method.
-        //You can read more about JSON here http://json.org
-        url = "http://api.linkedin.com/v1/people/~";
-        request = new OAuthRequest(Verb.GET, url);
-        request.addQuerystringParameter("format", "json");
-        service.signRequest(accessToken, request);
-        response = request.send();
-        System.out.println(response.getBody());
-        System.out.println();System.out.println();
-
-
-        System.out.println("********Get my connections - going into a resource********");
-        //This basic call gets all your connections each one will be in a person tag with some profile information
-        //https://developer.linkedin.com/documents/connections-api
-        url = "http://api.linkedin.com/v1/people/~/connections";
-        request = new OAuthRequest(Verb.GET, url);
-        service.signRequest(accessToken, request);
-        response = request.send();
-        System.out.println(response.getBody());
-        System.out.println();System.out.println();
-
-
-        System.out.println("********Get only 10 connections - using parameters********");
-        //This basic call gets only 10 connections  - each one will be in a person tag with some profile information
-        //https://developer.linkedin.com/documents/connections-api
-        //more basic about query strings in a URL here http://en.wikipedia.org/wiki/Query_string
-        url = "http://api.linkedin.com/v1/people/~/connections";
-        request = new OAuthRequest(Verb.GET, url);
-        request.addQuerystringParameter("count", "10");
-        service.signRequest(accessToken, request);
-        response = request.send();
-        System.out.println(response.getBody());
-        System.out.println();System.out.println();
-
-
-        System.out.println("********GET network updates that are CONN and SHAR********");
-        //This basic call get connection updates from your connections
-        //https://developer.linkedin.com/documents/get-network-updates-and-statistics-api
-        //specifics on updates  https://developer.linkedin.com/documents/network-update-types
-
-        url = "http://api.linkedin.com/v1/people/~/network/updates";
-        request = new OAuthRequest(Verb.GET, url);
-        request.addQuerystringParameter("type","SHAR");
-        request.addQuerystringParameter("type","CONN");
-        service.signRequest(accessToken, request);
-        response = request.send();
-        System.out.println(response.getBody());
-        System.out.println();System.out.println();
-
-
-        System.out.println("********People Search using facets and Encoding input parameters i.e. UTF8********");
-        //This basic call get connection updates from your connections
-        //https://developer.linkedin.com/documents/people-search-api#Facets
-        //Why doesn't this look like
-        //people-search?title=developer&location=fr&industry=4
-
-        //url = "http://api.linkedin.com/v1/people-search?title=D%C3%A9veloppeur&facets=location,industry&facet=location,fr,0";
-        url = "http://api.linkedin.com/v1/people-search:(people:(first-name,last-name,headline),facets:(code,buckets))";
-        request = new OAuthRequest(Verb.GET, url);
-        request.addQuerystringParameter("title", "Développeur");
-        request.addQuerystringParameter("facet", "industry,4");
-        request.addQuerystringParameter("facets", "location,industry");
-        System.out.println(request.getUrl());
-        service.signRequest(accessToken, request);
-        response = request.send();
-        System.out.println(response.getBody());
-        System.out.println();System.out.println();
-
-        /////////////////field selectors
-        System.out.println("********A basic user profile call with field selectors********");
-        //The ~ means yourself - so this should return the basic default information for your profile in XML format
-        //https://developer.linkedin.com/documents/field-selectors
-        url = "http://api.linkedin.com/v1/people/~:(first-name,last-name,positions)";
-        request = new OAuthRequest(Verb.GET, url);
-        service.signRequest(accessToken, request);
-        response = request.send();
-        System.out.println(response.getHeaders().toString());
-        System.out.println(response.getBody());
-        System.out.println();System.out.println();
-
-
-        System.out.println("********A basic user profile call with field selectors going into a subresource********");
-        //The ~ means yourself - so this should return the basic default information for your profile in XML format
-        //https://developer.linkedin.com/documents/field-selectors
-        url = "http://api.linkedin.com/v1/people/~:(first-name,last-name,positions:(company:(name)))";
-        request = new OAuthRequest(Verb.GET, url);
-        service.signRequest(accessToken, request);
-        response = request.send();
-        System.out.println(response.getHeaders().toString());
-        System.out.println(response.getBody());
-        System.out.println();System.out.println();
-
-
-        System.out.println("********A basic user profile call into a subresource return data in JSON********");
-        //The ~ means yourself - so this should return the basic default information for your profile
-        //https://developer.linkedin.com/documents/field-selectors
-        url = "https://api.linkedin.com/v1/people/~/connections:(first-name,last-name,headline)?format=json";
-        request = new OAuthRequest(Verb.GET, url);
-        service.signRequest(accessToken, request);
-        response = request.send();
-        System.out.println(response.getHeaders().toString());
-        System.out.println(response.getBody());
-        System.out.println();System.out.println();
-
-        System.out.println("********A more complicated example using postings into groups********");
-        //https://developer.linkedin.com/documents/field-selectors
-        //https://developer.linkedin.com/documents/groups-api
-        url = "http://api.linkedin.com/v1/groups/3297124/posts:(id,category,creator:(id,first-name,last-name),title,summary,creation-timestamp,site-group-post-url,comments,likes)";
-        request = new OAuthRequest(Verb.GET, url);
-        service.signRequest(accessToken, request);
-        response = request.send();
-        System.out.println(response.getHeaders().toString());
-        System.out.println(response.getBody());
-        System.out.println();System.out.println();
-
     }
 
     public static Token getAccess(OAuthService oAuthService) {
